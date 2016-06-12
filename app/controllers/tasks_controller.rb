845bdd61337762b37f6985cfa49571b5383d5c8c
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-06-12 23:36:21 +0000

    create task api

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,6 @@ class TasksController < ApplicationController
 
   private
     def task_params
-      params.require(:task).permit(:done, :title)
+      params.require(:task).permit(:done, :title )
     end
 end
